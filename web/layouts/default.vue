56eefb8ca37e927132cb53019dbc08240e3368a8
Breno <brenohenrique098@gmail.com>
2023-10-14 17:04:28 -0300

    To fix default layout

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,47 +1,66 @@
 <script lang="ts" setup>
   import { useAuthStore } from '~/stores/useAuthStore';
+  import { useShopingCart } from '~/stores/useShopingCart';
 
   const auth = useAuthStore();
+  const shopingCart = useShopingCart();
 
   async function handleLogout() {
     await auth.logout();
   }
+
+  const userName = computed(() => auth.user ? auth.user.name : null);
+
+  const countProductsInCart = computed(() => shopingCart.products.length)
+
+  function goToIndexPage() {
+    navigateTo(`/`)
+  }
+
+  function showShoppingCard() {
+    navigateTo(`/my-cart`)
+  }
 </script>
 
 <template>
-  <div>
-    <button
-      v-if="auth.isLoggedIn"
-      @click="handleLogout"
+   <q-layout view="lhh lpR lFf">
+    <q-header class="bg-green">
+      <q-toolbar>
+        <q-toolbar-title 
+          @click="goToIndexPage"
+          class="text-black row items-center" 
+        >
+          MenuOn
+        </q-toolbar-title>
+
+        {{ userName }}
+
+        <q-btn round dense flat @click="handleLogout">
+          <q-icon name="logout" color="black" size="xs"/>
+          <q-tooltip>Sair</q-tooltip>
+        </q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <q-page>
+        <slot />
+      </q-page>
+    </q-page-container>
+
+    <q-footer 
+      bordered 
+      class="transparent text-black q-px-lg flex justify-end"
     >
-      Logout
-    </button>
-
-    <pre>
-      {{ auth.user }}
-    </pre>
-
-    <q-btn color="amber" label="Amber" />
-
-    <ul>
-      <li>
-        <nuxt-link to="/">Home</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/register">Register</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/login">Login</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/auth-only">Auth Only</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/guest-only">Guest Only</nuxt-link>
-      </li>
-    </ul>
-    <slot />
-  </div>
+        <q-btn 
+          round  
+          icon="shopping_cart"
+          @click="showShoppingCard"
+        >
+          <q-badge color="orange" floating>
+            {{ countProductsInCart }}
+          </q-badge>
+        </q-btn>
+    </q-footer>
+   </q-layout>
 </template>
-
-<style scoped></style>
